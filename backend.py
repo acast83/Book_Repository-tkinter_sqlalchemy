e22848cec11360c1f27eae4d4eaf02139121d480
‘acast83’ <acast83@gmail.com>
2022-02-05 11:31:35 +0100

    files modified

--- backend.py
+++ backend.py
@@ -1,10 +1,28 @@
 from db_model import Db_Model, engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import func
+import logging
 
+# setting up a logger
+logger = logging.getLogger(
+    __name__
+)
+logger.setLevel(logging.DEBUG)
+
+file_handler = logging.FileHandler("book_repository.log")
+logger.addHandler(file_handler)
+
+formatter = logging.Formatter(
+    "%(asctime)s:%(created)f:%(levelname)s:%(message)s"
+)
+file_handler.setFormatter(formatter)
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+logger.addHandler(stream_handler)
 
 Session = sessionmaker(bind=engine)
 session = Session()
+session.expire_on_commit = False
 
 
 class Backend:
@@ -15,73 +33,109 @@ class Backend:
                             year=year_data, isbn=isbn_data)
             session.add(book)
             session.commit()
+            logger.info(
+                f"Successfully added new book to the repository, id {book.id}")
         except Exception as e:
-            print(f"Error, {e}")
+            logger.debug(f"Error, {e}")
         finally:
             session.close()
 
     def view(self,):
-        books = session.query(Db_Model).all()
-        session.close()
-        return books
+        try:
+            books = session.query(Db_Model).all()
+            session.close()
+            return books
+        except Exception as e:
+            logger.debug(f"Error, {e}")
 
     def delete(self, id):
-        book = session.query(Db_Model).filter(Db_Model.id == id).first()
-        session.delete(book)
-        session.commit()
-        session.close()
+        try:
+            book = session.query(Db_Model).filter(Db_Model.id == id).first()
+            session.delete(book)
+            session.commit()
+            session.close()
+            logger.info(
+                f"Successfully removed book from the repository, id {id}")
+        except Exception as e:
+            logger.debug(f"Error, {e}")
 
     def search(self, title_data, author_data, year_data, isbn_data):
-        if title_data != None:
-            if " " or "-" in title_data:
-                keywords = title_data.split()
-                book_list = session.query(Db_Model).all()
-                results = []
-                for keyword in keywords:
+        try:
+            if title_data != None:
+                if " " or "-" in title_data:
+                    keywords = title_data.split()
+                    book_list = session.query(Db_Model).all()
+                    results = []
+                    for keyword in keywords:
+                        for book in book_list:
+                            if keyword.lower() in (book.title).lower():
+                                results.append(book)
+                    if len(results) != 0:
+                        results = list(set(results))
+                        return results
+
+                else:
+                    book_list = session.query(Db_Model).all()
+                    results = []
                     for book in book_list:
-                        if keyword.lower() in (book.title).lower():
+                        if title_data in book.title:
                             results.append(book)
-                if len(results) != 0:
-                    results = list(set(results))
-                    return results
+                    if len(results) > 0:
+                        return results
 
-            else:
-                book_list = session.query(Db_Model).all()
-                results = []
-                for book in book_list:
-                    if title_data in book.title:
-                        results.append(book)
-                if len(results) > 0:
-                    return results
+            if author_data != None:
+                books = session.query(Db_Model).filter(
+                    func.lower(Db_Model.author) == func.lower(author_data)).all()
+                session.close()
+                if len(books) > 0:
+                    return books
 
-        if author_data != None:
-            books = session.query(Db_Model).filter(
-                func.lower(Db_Model.author) == func.lower(author_data)).all()
-            session.close()
-            if len(books) > 0:
-                return books
+            if year_data != None:
+                books = session.query(Db_Model).filter(
+                    Db_Model.year == year_data).all()
+                session.close()
+                if len(books) > 0:
+                    return books
 
-        if year_data != None:
-            books = session.query(Db_Model).filter(
-                Db_Model.year == year_data).all()
-            session.close()
-            if len(books) > 0:
-                return books
-
-        if isbn_data != None:
-            books = session.query(
-                Db_Model).filter(Db_Model.isbn == isbn_data).all()
-            session.close()
-            if len(books) > 0:
-                return books
-            else:
-                return
+            if isbn_data != None:
+                books = session.query(
+                    Db_Model).filter(Db_Model.isbn == isbn_data).all()
+                session.close()
+                if len(books) > 0:
+                    return books
+                else:
+                    return
+        except Exception as e:
+            logger.debug(f"Error, {e}")
 
     def update(self, id, title, author, year, isbn):
-        book = session.query(Db_Model).filter(Db_Model.id == id).first()
-        book.title = title
-        book.author = author
-        book.year = year
-        book.isbn = isbn
-        session.commit()
-        session.close()
+        try:
+            if title != None:
+                book = session.query(Db_Model).filter(
+                    Db_Model.id == id).first()
+                book.title = title
+                session.commit()
+                session.close()
+            if author != None:
+                book = session.query(Db_Model).filter(
+                    Db_Model.id == id).first()
+                book.author = author
+                session.commit()
+                session.close()
+            if year != None:
+                book = session.query(Db_Model).filter(
+                    Db_Model.id == id).first()
+                book.year = year
+                session.commit()
+                session.close()
+            if isbn != None:
+                book = session.query(Db_Model).filter(
+                    Db_Model.id == id).first()
+                book.isbn = isbn
+                session.commit()
+                session.close()
+            session.expunge_all()
+            session.close()
+            logger.info(f"Successfully mofified book entry, id {book.id}")
+        except Exception as e:
+            logger.debug(f"Error, {e}")
